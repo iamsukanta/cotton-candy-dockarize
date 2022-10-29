ba0050ba136cf03f6d3d6eb067d63f20bf6aef41
Sukanta Bala <sukantabalacste28@gmail.com>
2022-10-29 15:11:18 +0800

    Homepage updated for cotton candy frontend

---
+++ frontend/src/components/homepage/BannerBottomComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="flex flex-col md:flex-row items-center">
+    <div>
+      <a href="/Sustainable-2021">
+        <img src="/src/assets/images/bannerbottom/CC-SustainableProducts2021-Banner.jpg" alt="2021 Sustainable Products" title="2021 Sustainable Products">
+      </a>
+    </div>
+    <div>
+      <a href="https://catalog.cottoncandy.com">
+        <img src="/src/assets/images/bannerbottom/2022-Fall-Winter-Holiday-Banner-1.jpg" alt="2022 Fall and Winter Ideas" title="2022 Fall and Winter Ideas">
+      </a>
+    </div>
+    <div>
+      <a href="https://golf.cottoncandy.com">
+        <img src="/src/assets/images/bannerbottom/Golf2022-Banner4.jpg" alt="Golf 2022" title="Golf 2022">
+      </a>
+    </div>
+    <div>
+      <a href="/homefront-connection">
+        <img src="/src/assets/images/bannerbottom/HC-shift-down2.jpg" alt="Homefront Connection" title="Homefront Connection">
+      </a>
+    </div>
+  </section>
+</template>
