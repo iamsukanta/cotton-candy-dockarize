f1c1f6d7a530b14470ebfa4c1c6f632b9d9f5fdb
Sukanta Bala <sukantabalacste28@gmail.com>
2022-10-29 18:28:04 +0800

    HelpComponent updated

--- frontend/src/components/homepage/HelpComponent.vue
+++ frontend/src/components/homepage/HelpComponent.vue
@@ -2,65 +2,69 @@
   const grids = [
     {
       url: 'https://cottoncandy2.espwebsite.com/ProductResults/?SearchTerms=apparel&referrerPage=Home',
-      imgUrl: '/src/assets/images/help/Apparel_btn.jpg',
+      imgUrl: 'Apparel_btn.jpg',
       altText: 'Apparel button',
       new: true
     },
     {
       url: 'https://cottoncandy2.espwebsite.com/ProductResults/?SearchTerms=drinkware&referrerPage=Home',
-      imgUrl: '/src/assets/images/help/Drinkware_btn.jpg',
+      imgUrl: 'Drinkware_btn.jpg',
       altText: 'Drinkware button',
       new: false
     },
     {
       url: 'https://cottoncandy2.espwebsite.com/ProductResults/?SearchTerms=bags&referrerPage=Home',
-      imgUrl: '/src/assets/images/help/Bags_btn.jpg',
+      imgUrl: 'Bags_btn.jpg',
       altText: 'Bags button',
       new: false
     },
     {
       url: 'https://cottoncandy2.espwebsite.com/ProductResults/?SearchTerms=writing&referrerPage=Home',
-      imgUrl: '/src/assets/images/help/Writing-Instruments_btn.jpg',
+      imgUrl: 'Writing-Instruments_btn.jpg',
       altText: 'Writing Instruments button',
       new: true
     },
     {
       url: 'https://cottoncandy2.espwebsite.com/ProductResults/?SearchTerms=technology&referrerPage=Home',
-      imgUrl: '/src/assets/images/help/Technology_btn.jpg',
+      imgUrl: 'Technology_btn.jpg',
       altText: 'Technology button',
       new: false
     },
     {
       url: 'https://cottoncandy2.espwebsite.com/ProductResults/?SearchTerms=headwear&referrerPage=Home',
-      imgUrl: '/src/assets/images/help/Headwear-and-Accessories_btn.jpg',
+      imgUrl: 'Headwear-and-Accessories_btn.jpg',
       altText: 'Headwear and Accessories button',
       new: true
     },
     {
       url: 'https://cottoncandy2.espwebsite.com/ProductResults/?SearchTerms=events&referrerPage=Home',
-      imgUrl: '/src/assets/images/help/Events_btn.jpg',
+      imgUrl: 'Events_btn.jpg',
       altText: 'Events button',
       new: false
     },
     {
       url: 'https://cottoncandy2.espwebsite.com/ProductResults/?SearchTerms=outdoors&referrerPage=Home',
-      imgUrl: '/src/assets/images/help/Outdoors_btn.jpg',
+      imgUrl: 'Outdoors_btn.jpg',
       altText: 'Outdoors button',
       new: false
     },
     {
       url: 'https://cottoncandy2.espwebsite.com/ProductResults/?SearchTerms=travel&referrerPage=Home',
-      imgUrl: '/src/assets/images/help/Travel_btn.jpg',
+      imgUrl: 'Travel_btn.jpg',
       altText: 'Travel button',
       new: false
     },
     {
       url: 'https://cottoncandy2.espwebsite.com/ProductResults/?SearchTerms=golf&referrerPage=Home',
-      imgUrl: '/src/assets/images/help/Golf_btn.jpg',
+      imgUrl: 'Golf_btn.jpg',
       altText: 'Golf button',
       new: false
     }
-  ]
+  ];
+
+  const imageSrc = (value) => {
+    return new URL(`/src/assets/images/help/${value}`, import.meta.url);
+  };
 </script>
 
 <template>
@@ -75,12 +79,12 @@
       </div>
 
       <div class="grid grid-cols-1 sm:grid-cols-2">
-        <div class="p-2.5" v-for="(item, index) in grids" key="index">
+        <div class="p-2.5" v-for="(item, index) in grids" :key="index">
           <a class="relative group" :href="item.url" target="_blank" rel="noopener noreferrer nofollow">
             <div class="absolute w-[30px] h-[30px] bg-cultured rounded-full -right-1.5 -top-1.5 flex items-center justify-center" v-if="item.new">
               <span class="w-4 h-4 bg-[#79B40C] rounded-full"></span>
             </div>
-            <img class="w-full group-hover:opacity-70" :src="item.imgUrl" :alt="item.altText">
+            <img class="w-full group-hover:opacity-70" :src="imageSrc(item.imgUrl)" :alt="item.altText">
             <!-- <div class="group-hover:block hidden absolute inset-0 w-full h-full bg-black/70 text-white"></div> -->
           </a>
         </div>
